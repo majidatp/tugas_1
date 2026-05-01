7ad008205d7e1204fcb58883b19aaf1c6272a81f
majidatp <majidstp6@gmail.com>
2026-05-01 20:45:20 +0700

    save

--- app/Http/Controllers/NamaController.php
+++ app/Http/Controllers/NamaController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ChirpController extends Controller
-{ 
+class NamaController extends Controller
+{
     public function index()
-    { return view ('welcome');
+    {
+        return view("welcome");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\NamaController;
+Route::get('/', [NamaController::class, 'index']);
 
 Route::get('/user', [UserController::class, 'index']);
